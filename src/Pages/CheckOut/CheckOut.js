30219cf89b230d7b148e7cb0b4a6d2670c44d4ac
Shakil Ahamed <sakilahmed7810@gmail.com>
2022-11-06 06:33:03 +0600

    Order Added

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -24,6 +24,23 @@ const CheckOut = () => {
             phone,
             message
         }
+
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.acknowledged){
+                alert('Order Placed Successfully');
+                form.reset();
+            }
+        })
+        .catch(err => console.log(err));
     }
 
     return (
@@ -34,18 +51,18 @@ const CheckOut = () => {
             <form onSubmit={handleCheckOutrOrder}>
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 my-4'>
 
-                    <input name='firstName' type="text" placeholder="First Name" className="input input-bordered input-primary w-full" />
+                    <input name='firstName' type="text" placeholder="First Name" defaultValue={user?.displayName} className="input input-bordered input-primary w-full" required />
 
                     <input name='lastName' type="text" placeholder="Last Name" className="input input-bordered input-primary w-full" />
 
-                    <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered input-primary w-full" />
+                    <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered input-primary w-full" required />
 
                     <input name='email' type="text" placeholder="Your Email" className="input input-bordered input-primary w-full" readOnly defaultValue={user?.email} />
                 </div>
 
-                <textarea name='message' className="textarea textarea-secondary w-full" placeholder="Bio"></textarea>
+                <textarea name='message' className="textarea textarea-secondary w-full" placeholder="Your Message"></textarea>
 
-                <input className='btn my-4' type="button" value="Place Your Order" />
+                <input className='btn my-4' type="submit" value="Place Your Order" />
             </form>
         </div>
     );
